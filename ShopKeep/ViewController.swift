7707a42cfac6e91ee6c9e6a36ba14cc6e15259cb
Alex Fu <alexfu@thelookingglass.io>
2018-11-17 08:51:40 -0800

    Initial Commit

---
+++ ShopKeep/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShopKeep
+//
+//  Created by Alex Fu on 11/17/18.
+//  Copyright © 2018 Alex Fu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
