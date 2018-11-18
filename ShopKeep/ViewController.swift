948e10f6d8b43191eeebf6be86ce22a00405378b
Alex Fu <alexfu@thelookingglass.io>
2018-11-17 18:42:39 -0600

    store view

--- ShopKeep/ViewController.swift
+++ ShopKeep/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var storeView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // corner radius
+        storeView.layer.cornerRadius = 20
+        
+        // border
+//        storeView.layer.borderWidth = 1.0
+//        storeView.layer.borderColor = UIColor.black.cgColor
+        
+        // shadow
+//        storeView.layer.shadowColor = UIColor.black.cgColor
+//        storeView.layer.shadowOffset = CGSize(width: 3, height: 3)
+//        storeView.layer.shadowOpacity = 0.7
+//        storeView.layer.shadowRadius = 4.0
     }
 
 
