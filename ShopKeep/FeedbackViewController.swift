1433592d73051cb0a2ddd12cd5a18bdf38cf468f
Alex Fu <alexfu@thelookingglass.io>
2018-12-08 14:04:44 -0800

    feedback view draft done

--- ShopKeep/FeedbackViewController.swift
+++ ShopKeep/FeedbackViewController.swift
@@ -9,5 +9,21 @@
 import UIKit
 
 class FeedbackViewController: UIViewController {
+    @IBOutlet weak var headshotImageView: UIImageView!
+    @IBOutlet weak var feedbackTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // headshot
+        headshotImageView.layer.cornerRadius = headshotImageView.frame.size.width / 2
+        headshotImageView.clipsToBounds = true
+        headshotImageView.layer.borderWidth = 8.0
+        headshotImageView.layer.borderColor = UIColor.white.cgColor
+        
+        // textview
+        feedbackTextView.layer.borderWidth = 1.0
+        feedbackTextView.layer.borderColor = UIColor.gray.cgColor
+    }
     
 }
